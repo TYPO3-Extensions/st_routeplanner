9e7c008231acfcec43f4b60a72d7707804cdfa78
scheibo <scheibo@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-04-18 20:10:03 +0000

    0.2.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/st_routeplanner/tags/0.2.1@74627 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "st_routeplanner".
-#
-# Auto generated 09-08-2012 21:30
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "st_routeplanner".
+ *
+ * Auto generated 18-04-2013 22:06
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Easy route planner',
 	'description' => 'a simple route planner - Google Maps',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.1.0',
+	'version' => '0.2.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,13 +34,16 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '4.5.0-6.0.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"82c2";s:10:"README.txt";s:4:"9d70";s:12:"ext_icon.gif";s:4:"5ff2";s:17:"ext_localconf.php";s:4:"8fe4";s:14:"ext_tables.php";s:4:"0cba";s:12:"flexform.xml";s:4:"3f32";s:16:"locallang_db.xml";s:4:"878b";s:17:"locallang_tca.php";s:4:"3e39";s:19:"doc/wizard_form.dat";s:4:"c204";s:20:"doc/wizard_form.html";s:4:"d72e";s:35:"pi1/class.tx_strouteplanner_pi1.php";s:4:"81ac";s:17:"pi1/locallang.xml";s:4:"f461";s:25:"static/st_routeplanner.js";s:4:"0637";s:16:"static/style.css";s:4:"a1df";s:20:"static/template.html";s:4:"e7d0";s:36:"static/st_routeplanner/constants.txt";s:4:"d41d";s:32:"static/st_routeplanner/setup.txt";s:4:"1d37";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"82c2";s:12:"ext_icon.gif";s:4:"5ff2";s:17:"ext_localconf.php";s:4:"8fe4";s:14:"ext_tables.php";s:4:"0cba";s:12:"flexform.xml";s:4:"3f32";s:16:"locallang_db.xml";s:4:"878b";s:17:"locallang_tca.php";s:4:"3e39";s:10:"README.txt";s:4:"9d70";s:19:"doc/wizard_form.dat";s:4:"c204";s:20:"doc/wizard_form.html";s:4:"d72e";s:35:"pi1/class.tx_strouteplanner_pi1.php";s:4:"b376";s:17:"pi1/locallang.xml";s:4:"f461";s:25:"static/st_routeplanner.js";s:4:"65ba";s:16:"static/style.css";s:4:"a1df";s:20:"static/template.html";s:4:"e7d0";s:36:"static/st_routeplanner/constants.txt";s:4:"d41d";s:32:"static/st_routeplanner/setup.txt";s:4:"1d37";}',
+	'suggests' => array(
+	),
 );
 
 ?>
--- static/st_routeplanner.js
+++ static/st_routeplanner.js
@@ -2,74 +2,71 @@ var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 
 $(document).ready(function() {
-	
+
 	function initialize() {
 		directionsDisplay = new google.maps.DirectionsRenderer();
 		var map = new google.maps.Map(document.getElementById('map_canvas'),myOptions);
 		directionsDisplay.setMap(map);
 		directionsDisplay.setPanel(document.getElementById('directions-panel'));
-	        
-	    var infowindow = new google.maps.InfoWindow({
-	        content: contentString,
-	        maxWidth: 300
-	    });
-	
-	    var marker = new google.maps.Marker({
-	        position: myLatlng,
-	        map: map
-	    });
-	    
-	    google.maps.event.addListener(marker, 'click', function() {
-	      infowindow.open(map,marker);
-	    });
-	    
-	    $('#st_routeplanner_submit').live('click',function(e){
+
+		var infowindow = new google.maps.InfoWindow({
+			content: contentString,
+			maxWidth: 300
+		});
+
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map
+		});
+
+		google.maps.event.addListener(marker, 'click', function() {
+		  infowindow.open(map,marker);
+		});
+
+		$(document).on('click', '#st_routeplanner_submit', function (e) {
 			if($('#st_routeplanner_start').val()) {
 				infowindow.close(map,marker);
 				marker.setMap(null);
 			}
 		});
-		
 	}
-	
+
 	function calcRoute(start,end) {
 		var request = {
-	  		origin: start,
-	  		destination: end,
-	  		travelMode: google.maps.DirectionsTravelMode.DRIVING
+			origin: start,
+			destination: end,
+			travelMode: google.maps.DirectionsTravelMode.DRIVING
 		};
 		directionsService.route(request, function(response, status) {
-	  		if (status == google.maps.DirectionsStatus.OK) {
-	    		directionsDisplay.setDirections(response);
-	  		}
+			if (status == google.maps.DirectionsStatus.OK) {
+				directionsDisplay.setDirections(response);
+			}
 		});
 	}
-	
+
 	initialize();
-      
-	$('#st_routeplanner_submit').live('click',function(e){
+
+	$(document).on('click', '#st_routeplanner_submit', function (e) {
 		if($('#st_routeplanner_start').val()) {
 			calcRoute($('#st_routeplanner_start').val(),$('#st_routeplanner_end').html());
 		}
 	});
-	
+
 	$('#st_routeplanner_start').bind('focus', function() {
 		oldValue = $(this).val();
 		$(this).val('');
 	});
-	
+
 	$('#st_routeplanner_start').bind('blur', function() {
 		newValue = $(this).val();
 		if (newValue == '') {
 			$(this).val(oldValue);
 		}
 	});
-	
+
 	$('#st_routeplanner_start').keypress(function(e){
 		if(e.which == 13){
-       		calcRoute($('#st_routeplanner_start').val(),$('#st_routeplanner_end').html());
-       	}
+			calcRoute($('#st_routeplanner_start').val(),$('#st_routeplanner_end').html());
+		}
 	});
-
-	
 });
