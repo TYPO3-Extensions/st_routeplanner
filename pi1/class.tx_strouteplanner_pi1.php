d2121f9564dd033d7ea50805c5790de1cced14db
scheibo <scheibo@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-04-18 19:32:13 +0000

    fixed googlemaps coordinates calculation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/st_routeplanner/trunk@74624 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_strouteplanner_pi1.php
+++ pi1/class.tx_strouteplanner_pi1.php
@@ -55,10 +55,9 @@ class tx_strouteplanner_pi1 extends tslib_pibase {
 		$this->pi_setPiVarDefaults();
 		$this->pi_loadLL();
 		$this->pi_initPIflexForm();
-			
-		
+
 		foreach ($this->cObj->data['pi_flexform']['data']['sDEF']['lDEF'] as $key => $value) $$key = reset($value);
-		
+
 		$langkey = $GLOBALS['TSFE']->config['config']['language'];
 
 		foreach (explode(',', 'maptype,zoom,mapwidth,mapheight,destination,infotext,template') as $value) $$value  = ($this->conf[$value]) ? $this->conf[$value] : $$value;
@@ -66,46 +65,57 @@ class tx_strouteplanner_pi1 extends tslib_pibase {
 		$sensor 					= ($this->conf['sensor']) 			? $this->conf['sensor'] 			: 'true';
 		$destination_name 			= ($this->conf['destinationname']) 	? $this->conf['destinationname'] 	: trim(end(explode('|', $destination)));
 		
-		$GLOBALS['TSFE']->additionalHeaderData[$this->extKey.'_10']	= '<link href="'.t3lib_extMgm::siteRelPath( $this->extKey ).'static/style.css" rel="stylesheet" type="text/css" />';
-		$GLOBALS['TSFE']->additionalFooterData[$this->extKey.'_100']	= '<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor='.$sensor.'&amp;language='.$langkey.'"></script>';
-		$GLOBALS['TSFE']->additionalFooterData[$this->extKey.'_120']	= '<script type="text/javascript" src="'.t3lib_extMgm::siteRelPath( $this->extKey ).'static/st_routeplanner.js"></script>';
+		$GLOBALS['TSFE']->additionalHeaderData[$this->extKey.'_10']	= '<link href="' . t3lib_extMgm::siteRelPath( $this->extKey ) . 'static/style.css" rel="stylesheet" type="text/css" />';
+		$GLOBALS['TSFE']->additionalFooterData[$this->extKey.'_100']	= '<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=' . $sensor . '&amp;language=' . $langkey . '"></script>';
+		$GLOBALS['TSFE']->additionalFooterData[$this->extKey.'_120']	= '<script type="text/javascript" src="' . t3lib_extMgm::siteRelPath( $this->extKey ) . 'static/st_routeplanner.js"></script>';
 
-		if($destination_coordinates == 'error1') return '<div class="tx_strouteplanner_error">'.$start.'&nbsp;-&nbsp;'.$this->pi_getLL('location_error').'</div>';
-		if($destination_coordinates == 'error2') return '<div class="tx_strouteplanner_error">'.$this->pi_getLL('spam_error').'</div>';
+		if($destination_coordinates == 'error1') return '<div class="tx_strouteplanner_error">' . $start . '&nbsp;-&nbsp;' . $this->pi_getLL('location_error') . '</div>';
+		if($destination_coordinates == 'error2') return '<div class="tx_strouteplanner_error">' . $this->pi_getLL('spam_error') . '</div>';
 
-		
-		$GLOBALS['TSFE']->additionalFooterData[$this->extKey.'_110']	= '<script type="text/javascript">
-			var myLatlng = new google.maps.LatLng('.$destination_coordinates.');
+		$GLOBALS['TSFE']->additionalFooterData[$this->extKey . '_110']	= '<script type="text/javascript">
+			var myLatlng = new google.maps.LatLng(' . $destination_coordinates[0] . ',' . $destination_coordinates[1] . ');
 			var myOptions = {
-	      		zoom: '.$zoom.',
-	      		mapTypeId: google.maps.MapTypeId.'.strtoupper($maptype).',
-	      		center: new google.maps.LatLng('.$destination_coordinates.')
+	      		zoom: ' . $zoom . ',
+	      		mapTypeId: google.maps.MapTypeId.' . strtoupper($maptype) . ',
+	      		center: new google.maps.LatLng(' . $destination_coordinates[0] . ',' . $destination_coordinates[1] . ')
     		};
-			var contentString = \'<div id="content">'.preg_replace('/\r\n|\r|\n/', ' ', nl2br($infotext)).'</div>\';
+			var contentString = \'<div id="content">' . preg_replace('/\r\n|\r|\n/', ' ', nl2br($infotext)) . '</div>\';
 		</script>';
 
-		$markerArray['###START###'] 		= '<label>'.$this->pi_getLL('start').'</label><input id="st_routeplanner_start" type="text" name="start" value="" />';
-		$markerArray['###DESTINATION###'] 	= '<label>'.$this->pi_getLL('destination').'</label><span>'.$destination_name.'</span><div id="st_routeplanner_end">'.$destination_coordinates.'</div>';
-		$markerArray['###SUBMIT###']		= '<input type="submit" name="submit" id="st_routeplanner_submit" value="'.$this->pi_getLL('getDirections').'" />';
-		$markerArray['###MAP###']			= '<div id="map_canvas" style="width: '.$mapwidth.'px; height: '.$mapheight.'px"></div>';
-		$markerArray['###DIRECTION###']		= '<div id="directions-panel" style="width: '.$mapwidth.'px"></div>';
+		$markerArray['###START###'] 		= '<label>' . $this->pi_getLL('start') . '</label><input id="st_routeplanner_start" type="text" name="start" value="" />';
+		$markerArray['###DESTINATION###'] 	= '<label>' . $this->pi_getLL('destination') . '</label><span>' . $destination_name . '</span><div id="st_routeplanner_end">' . $destination_coordinates[0] . ',' . $destination_coordinates[1] . '</div>';
+		$markerArray['###SUBMIT###']		= '<input type="submit" name="submit" id="st_routeplanner_submit" value="' . $this->pi_getLL('getDirections') . '" />';
+		$markerArray['###MAP###']			= '<div id="map_canvas" style="width: ' . $mapwidth . 'px; height: ' . $mapheight . 'px"></div>';
+		$markerArray['###DIRECTION###']		= '<div id="directions-panel" style="width: ' . $mapwidth . 'px"></div>';
 	
 		
 		$template = ($template) ? $template : 'EXT:st_routeplanner/static/template.html' ;
 		$this->template=$this->cObj->fileResource($template);
 		$subpart=$this->cObj->getSubpart($this->template,'###ROUTEPLANNER###'); 
-		$content = $this->cObj->substituteMarkerArrayCached($subpart,$markerArray,$subpartArray,array());
+		$content = $this->cObj->substituteMarkerArrayCached($subpart, $markerArray, $subpartArray, array());
 		return $this->pi_wrapInBaseClass($content);
 	}
 
 
+	/**
+	 * Get geo coordinates from address
+	 *
+	 * @param string $data
+	 * @return array lat, lng
+	 */
 	function getMapsCoordinates($data){
-		$response = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($data).'&sensor=true');
-		$response = json_decode($response);
-		$lat = $response->results[0]->geometry->location->lat;
-		$lng = $response->results[0]->geometry->location->lng;
-		return $coordinate = $lat.','.$lng;
+		$json = t3lib_div::getUrl('https://maps.googleapis.com/maps/api/geocode/json?sensor=false&region=de&address=' . urlencode($data));
+		$jsonDecoded = json_decode($json, true);
+		if (!empty($jsonDecoded['results'])) {
+			$lat = $jsonDecoded['results']['0']['geometry']['location']['lat'];
+			$lng = $jsonDecoded['results']['0']['geometry']['location']['lng'];
+		} else {
+			$lat = 0;
+			$lng = 0;
+		}
+		return array($lat, $lng);
 	}
+
 }
 
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/st_routeplanner/pi1/class.tx_strouteplanner_pi1.php'])	{
